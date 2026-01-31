f8ff350c176d9a7db968f77f5d732bcbe5ed1cb7
lpc <429789530@qq.com>
2026-01-31 15:40:02 +0800

    feat:添加User项，加入user查询接口

--- src/main/java/org/example/test1/Test1Application.java
+++ src/main/java/org/example/test1/Test1Application.java
@@ -1,9 +1,11 @@
 package org.example.test1;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.example.test1.mapper")
 public class Test1Application {
 
     public static void main(String[] args) {
